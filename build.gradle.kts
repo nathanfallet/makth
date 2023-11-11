62fd3912408dccb935e434c91cc35b24efce1a22
NathanFallet <contact@nathanfallet.me>
2023-11-11 11:27:15 +0100

    Supporting more native targets

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,43 @@
 plugins {
-    kotlin("multiplatform") version "1.9.10"
+    kotlin("multiplatform") version "1.9.20"
     id("convention.publication")
     id("org.jetbrains.kotlinx.kover") version "0.7.4"
-    id("com.google.devtools.ksp") version "1.9.10-1.0.13"
+    id("com.google.devtools.ksp") version "1.9.20-1.0.13"
     id("dev.petuska.npm.publish") version "3.4.1"
 }
 
 group = "me.nathanfallet.makth"
-version = "1.2.2"
+version = "1.2.3"
 
 repositories {
     mavenCentral()
 }
 
 kotlin {
+    // Tiers are in accordance with <https://kotlinlang.org/docs/native-target-support.html>
+    // Tier 1
+    macosX64()
+    macosArm64()
+    iosSimulatorArm64()
+    iosX64()
+
+    // Tier 2
+    linuxX64()
+    linuxArm64()
+    watchosSimulatorArm64()
+    watchosX64()
+    watchosArm32()
+    watchosArm64()
+    tvosSimulatorArm64()
+    tvosX64()
+    tvosArm64()
+    iosArm64()
+
+    // Tier 3
+    mingwX64()
+    watchosDeviceArm64()
+
+    // jvm & js
     jvm {
         jvmToolchain(19)
         withJava()
@@ -29,19 +53,8 @@ kotlin {
         browser()
         //generateTypeScriptDefinitions() // Not supported for now because of collections etc...
     }
-    val hostOs = System.getProperty("os.name")
-    val isArm64 = System.getProperty("os.arch") == "aarch64"
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" && isArm64 -> macosArm64("native")
-        hostOs == "Mac OS X" && !isArm64 -> macosX64("native")
-        hostOs == "Linux" && isArm64 -> linuxArm64("native")
-        hostOs == "Linux" && !isArm64 -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
 
-
+    applyDefaultHierarchyTemplate()
     sourceSets {
         all {
             languageSettings.apply {
